dd3578d3935378bba78d664a2223933b690dfc0c
Arghya Guha <guha.agt@gmail.com>
2019-09-21 06:30:44 +0900

    simple working mock

--- App.js
+++ App.js
@@ -4,33 +4,61 @@ import {
   Button,
   View,
   SafeAreaView,
-  Text,
+  //Text,
   Alert
 } from "react-native";
 
 import Constants from "expo-constants";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert("Simple Button pressed")}
-        />
-      </View>
-    </SafeAreaView>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      en_word: "Yes",
+      jp_word: "Hai",
+      now_word: "Hai"
+    };
+    this.flashMe = this.flashMe.bind(this);
+  }
+
+  flashMe(e) {
+    e.preventDefault();    
+    this.setState({
+      now_word: this.state.en_word
+    })
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.pressButtonSection}>
+          <Button
+            title={this.state.now_word}
+            color="deeppink"            
+            onPress={this.flashMe}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16
+    marginHorizontal: 16,
+    backgroundColor: "black"
   },
   title: {
     textAlign: "center",
-    marginVertical: 8
+    marginVertical: 8    
+  },
+  pressButtonSection: {
+    marginTop: "100%",
+    marginLeft: "20%",
+    width: "60%",
+    height: "50%",
+    justifyContent: "center"
   }
 });
